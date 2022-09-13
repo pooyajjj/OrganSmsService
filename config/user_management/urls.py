86050cc838a8a87787339374afe00a115d73fe80
King <king@kali.King>
2022-09-13 20:16:33 +0430

    user management handeling 1.0.1

---
+++ config/user_management/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import Login
+
+
+
+
+urlpatterns = [
+    path('', Login.as_view(), name = 'login')
+]
---
+++ config/user_management/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate
+
+# Create your views here.
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'tmp/login.html', {'form': AuthenticationForm })
+
+    def post(self, request):
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            user = authenticate(
+                request,
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password')
+            )
+
+            if user is None :
+                return render(
+
+                    request,
+                    'tmp/login.html',
+                    { 'form': form, 'invalid_creds': True }
+
+                )
+
+            try:
+                form.confirm_login_allowed(user)
+
+            except ValidationError:
+
+                return render(
+                    request,
+                    'tmp/login.html',
+                    { 'form': form, 'invalid_creds': True }
+
+                )
+
+
+            return redirect('/organsmspanel')
+        
+        return render (request,'tmp/login.html',{
+        'help_text': True, 
+        'form': AuthenticationForm
+        })
