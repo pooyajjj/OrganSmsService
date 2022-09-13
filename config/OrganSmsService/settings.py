19791f8b36e629bd808cedfe8b284f8967d4c70b
King <king@kali.King>
2022-09-14 02:32:19 +0430

    user management handeling 1.0.2(template handling)

--- config/OrganSmsService/settings.py
+++ config/OrganSmsService/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +54,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'OrganSmsService.urls'
+
+
 
 TEMPLATES = [
     {
@@ -126,3 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
