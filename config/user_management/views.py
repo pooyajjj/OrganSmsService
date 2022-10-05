bc14a54b38fd390cdc5a3d2f781fcadc4ceb1e94
King <king@kali.King>
2022-10-05 19:56:47 +0330

    Finally login was set

--- config/user_management/views.py
+++ config/user_management/views.py
@@ -9,12 +9,14 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request = request, data = request.POST)
         if form.is_valid():
-            username = form.cleaned_data.GET['Username']
-            password = form.cleaned_data.GET['Password']
-            user = authenticate(request, username=username, password=password)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username = username, password = password)
             if user is not None:
                 login(request, user)
                 return redirect('/ok/')
+        else :
+            return redirect('/../signup/')
     
     form = AuthenticationForm()
     context = {'form':form}
