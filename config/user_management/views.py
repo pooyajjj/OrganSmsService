397d7dc35b2a8c1d6eb199cff13276d31deb815b
King <king@kali.King>
2022-10-07 01:47:57 +0330

    signup fixed

--- config/user_management/views.py
+++ config/user_management/views.py
@@ -2,41 +2,46 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views import View
-
+from .forms import SignUpForm
 
 
 def login_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request = request, data = request.POST)
+        form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(request, username = username, password = password)
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('/ok/')
-        else :
+        else:
             return redirect('/../signup/')
-    
-    form = UserCreationForm()
-    context = {'form':form}
+
+    form = AuthenticationForm()
+    context = {'form': form}
     return render(request, 'tmp/login.html', context)
 
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
+        print("-----")
+        print("form.is_valid():", form.is_valid(), form.errors)
+
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username = username, password = password)
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('/ok/')
         else :
             return redirect('/../no/')
-    return render(request, 'tmp/signup.html')
+    form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'tmp/signup.html', context)
 
 # class base signup
 # class signup_view(View):
@@ -56,5 +61,3 @@ def signup_view(request):
 #         else :
 #             return redirect ('/../fuck/')
 #         # return render(request, 'tmp/signup.html', {'form': UserCreationForm()})
-
-
